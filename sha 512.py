3f0ee98339fdbcc9c90e2c4106acdee9bfbcca5f
Sathwika akkala <168328158+SathwikaAkkala@users.noreply.github.com>
2024-10-18 15:51:56 +0530

    Add files via upload

---
+++ sha 512.py
@@ -0,0 +1,17 @@
+0000000import hashlib
+
+def sha512_hash_string(input_string):
+    # Create a SHA-512 hash object
+    sha512_hash = hashlib.sha512()
+    
+    # Update the hash object with the bytes of the input string
+    sha512_hash.update(input_string.encode('utf-8'))
+    
+    # Get the hexadecimal representation of the hash
+    return sha512_hash.hexdigest()
+
+# Example usage
+if __name__ == "__main__":
+    user_input = input("Enter a string to hash using SHA-512 : ")
+    hashed_output = sha512_hash_string(user_input)
+    print(f"SHA-512 hash of  '{user_input}' : {hashed_output}")
